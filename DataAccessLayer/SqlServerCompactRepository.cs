9393098377c959d894a95020244997f7e5c5134f
bherring_paucar <bpaucar@inventarte.net>
2018-05-04 09:24:19 -0500

    first commit

---
+++ DataAccessLayer/SqlServerCompactRepository.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer;
+
+namespace DataAccessLayer
+{
+    public class SqlServerCompactRepository : IRepository
+	{
+		public int SaveSpeaker(Speaker speaker)
+		{
+			//TODO: Save speaker to DB for now. For demo, just assume success and return 1.
+			return 1;
+		}
+	}
+}
